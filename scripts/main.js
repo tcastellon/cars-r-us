59c0c81215cb4f3157f131b28ad5262ff8d9f1d5
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-08-22 17:49:46 -0500

    Added tech and wheel drop down

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,17 @@
 import { colorOptions } from "./paintColorSelection.js"
 import { interiorOptions } from "./interiorSelection.js"
+import { techOptions } from "./techPackageSelection.js"
+import { wheelOptions } from "./wheelSelection.js"
+
 
 const container = document.querySelector("#container")
 
 const render = async () => {
     const colorOptionsHTML = await colorOptions()
     const interiorOptionsHTML = await interiorOptions()
-
+    const techOptionsHTML = await techOptions()
+    const wheelOptionsHTML = await wheelOptions()
+    
     const html = `
     <h1>Select Car Options</h1>
         <article class="car-options">
@@ -19,6 +24,18 @@ const render = async () => {
             <section class="interior-options">
                 <h2>Interiors</h2>
                     ${interiorOptionsHTML}
+            </section>
+        </article>
+        <article class="car-options">
+            <section class="tech-options">
+                <h2>Technology Packages</h2>
+                    ${techOptionsHTML}
+            </section>
+        </article>
+        <article class="car-options">
+            <section class="wheel-options">
+                <h2>Wheels</h2>
+                    ${wheelOptionsHTML}
             </section>
         </article>
         `
