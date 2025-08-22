1ed2781eb95b3572568d99d6e96564f343cdf06f
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-08-22 15:48:50 -0500

    Added interior drop down

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
 import { colorOptions } from "./paintColorSelection.js"
+import { interiorOptions } from "./interiorSelection.js"
 
 const container = document.querySelector("#container")
 
 const render = async () => {
     const colorOptionsHTML = await colorOptions()
+    const interiorOptionsHTML = await interiorOptions()
 
     const html = `
     <h1>Select Car Options</h1>
@@ -12,7 +14,14 @@ const render = async () => {
                 <h2>Colors</h2>
                     ${colorOptionsHTML}
             </section>
-        </article>`
+        </article>
+        <article class="car-options">
+            <section class="interior-options">
+                <h2>Interiors</h2>
+                    ${interiorOptionsHTML}
+            </section>
+        </article>
+        `
     
     container.innerHTML = html
 }
