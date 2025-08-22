97485acb29186bdd909aff428e9977b5a99d4e67
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-08-22 17:48:07 -0500

    Added technology options drop down generator function

---
+++ scripts/techPackageSelection.js
@@ -0,0 +1,14 @@
+export const techOptions = async () => {
+    const response = await fetch("http://localhost:8088/technologyPackages")
+    const techPackages = await response.json()
+
+    let techPackagesHTML = `
+        <select id="technology">
+            <option value="0">Select Option</option>`
+
+    for (const tech of techPackages) {
+        techPackagesHTML += `<option value="${tech.id}">${tech.package}</option>`
+    }
+    techPackagesHTML += `</select>`
+    return techPackagesHTML
+}
