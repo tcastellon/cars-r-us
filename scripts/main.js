980e3eb4560fff4b26f421d8b1228fd6603aa7db
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-08-26 09:14:13 -0500

    Updated main

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,8 @@ import { colorOptions } from "./paintColorSelection.js"
 import { interiorOptions } from "./interiorSelection.js"
 import { techOptions } from "./techPackageSelection.js"
 import { wheelOptions } from "./wheelSelection.js"
-
+import { submissionButton } from "./placeOrderButton.js"
+import { orderList } from "./ordersPlacedList.js"
 
 const container = document.querySelector("#container")
 
@@ -11,36 +12,52 @@ const render = async () => {
     const interiorOptionsHTML = await interiorOptions()
     const techOptionsHTML = await techOptions()
     const wheelOptionsHTML = await wheelOptions()
+    const buttonHTML = submissionButton()
+    const ordersHTML = await orderList()
     
     const html = `
     <h1>Select Car Options</h1>
-        <article class="car-options">
+        <div class="car-options">
             <section class="color-options">
                 <h2>Colors</h2>
                     ${colorOptionsHTML}
             </section>
-        </article>
-        <article class="car-options">
+        </div>
+        <div class="car-options">
             <section class="interior-options">
                 <h2>Interiors</h2>
                     ${interiorOptionsHTML}
             </section>
-        </article>
-        <article class="car-options">
+        </div>
+        <div class="car-options">
             <section class="tech-options">
                 <h2>Technology Packages</h2>
                     ${techOptionsHTML}
             </section>
-        </article>
-        <article class="car-options">
+        </div>
+        <div class="car-options">
             <section class="wheel-options">
                 <h2>Wheels</h2>
                     ${wheelOptionsHTML}
             </section>
-        </article>
+        </div>
+        <div class="button">
+            ${buttonHTML}
+        </div>
+        <div class="orders-list">
+            <section class="orders">
+                <h2>Orders</h2>
+                    ${ordersHTML}
+            </section>
+        </div>
         `
     
     container.innerHTML = html
 }
+
+document.addEventListener("newOrderPlaced", event => {
+    console.log("State of data has changed. Regenerating HTML...")
+    render()
+})
 
 render()
